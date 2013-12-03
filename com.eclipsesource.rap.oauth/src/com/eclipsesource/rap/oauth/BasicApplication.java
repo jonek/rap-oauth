bb2de3b8fe41f3c3e37b4042acb88e644c213b30
Johannes Eickhold <jeick@eclipsesource.com>
2013-12-03 08:17:56 +0100

    Initial commit of RAP Hello World skeleton and libs
    Google API taken from
    http://google-oauth-java-client.googlecode.com/files/google-oauth-java-client-1.17.0-rc.zip

---
+++ com.eclipsesource.rap.oauth/src/com/eclipsesource/rap/oauth/BasicApplication.java
@@ -0,0 +1,20 @@
+
+package com.eclipsesource.rap.oauth;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.rap.rwt.application.Application;
+import org.eclipse.rap.rwt.application.ApplicationConfiguration;
+import org.eclipse.rap.rwt.client.WebClient;
+
+public class BasicApplication implements ApplicationConfiguration {
+
+  @Override
+  public void configure( Application application ) {
+    Map<String, String> properties = new HashMap<String, String>();
+    properties.put( WebClient.PAGE_TITLE, "OAuth Demo" );
+    application.addEntryPoint( "/auth", BasicEntryPoint.class, properties );
+  }
+
+}
